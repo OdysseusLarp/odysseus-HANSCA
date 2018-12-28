70b152060f8f1700d64d47cd9d03a6b404c1ec53
Janne Koponen <janne.koponen@wunder.io>
2018-12-28 10:26:19 +0200

    Add PWA scaffold.

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+var CACHE_NAME = 'hansca-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
