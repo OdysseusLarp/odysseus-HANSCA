7ab630f6782810bc8c498f03ad7eb59c57be38ad
Sampo Juustila <sampo.juustila@iki.fi>
2018-12-31 09:15:51 +0200

    Add cross-delta coefficients

--- src/components/games/PhaseSyncGame.vue
+++ src/components/games/PhaseSyncGame.vue
@@ -1,28 +1,10 @@
 <template>
     <div>
         <phase-sync-graph :class="['graph', success ? 'success':'']" :graphs="graphs"></phase-sync-graph>
-
         <div v-for="(t, index) in target" :key="index">
             <p>{{current[index]}} / {{target[index]}}</p>
             <v-ons-range v-model="current[index]" min="0" max="1" step="0.001" style="width: 100%"></v-ons-range>
         </div>
-
-        <p>amplitude: {{graphs[0].amplitude}}</p>
-        <v-ons-range v-model="graphs[0].amplitude" min="0" max="2" step="0.001" style="width: 100%;"></v-ons-range>
-        <p>wavelength: {{graphs[0].wavelength}}</p>
-        <v-ons-range v-model="graphs[0].wavelength" min="0" max="2" step="0.001" style="width: 100%;"></v-ons-range>
-        <p>phaseSpeed: {{graphs[0].phaseSpeed}}</p>
-        <v-ons-range v-model="graphs[0].phaseSpeed" min="-10" max="10" step="0.001" style="width: 100%;"></v-ons-range>
-        <v-ons-checkbox v-model="graphs[0].lock"></v-ons-checkbox> Locked
-
-        <p>amplitude: {{graphs[1].amplitude}}</p>
-        <v-ons-range v-model="graphs[1].amplitude" min="0" max="2" step="0.001" style="width: 100%;"></v-ons-range>
-        <p>wavelength: {{graphs[1].wavelength}}</p>
-        <v-ons-range v-model="graphs[1].wavelength" min="0" max="2" step="0.001" style="width: 100%;"></v-ons-range>
-        <p>phaseSpeed: {{graphs[1].phaseSpeed}}</p>
-        <v-ons-range v-model="graphs[1].phaseSpeed" min="-10" max="10" step="0.001" style="width: 100%;"></v-ons-range>
-        <v-ons-checkbox v-model="graphs[1].lock"></v-ons-checkbox> Locked
-
     </div>
 </template>
 
@@ -63,8 +45,13 @@ export default {
         const targetWavelength = 0.25 + 0.5 * Math.random()
 
         for (var i = 0; i < this.dimensions; i++) {
-            target.push(Math.random())
-            current.push(Math.random())
+            let t = Math.random()
+            let c = Math.random()
+            while (Math.abs(t-c) < 0.15) {
+                c = Math.random()
+            }
+            target.push(t)
+            current.push(c)
             amplitudeBase.push(0.65 + 0.1 * Math.random())
             amplitudeCoefficient.push(0.1 * Math.random())
             wavelengthBase.push(targetWavelength)
@@ -73,9 +60,9 @@ export default {
             const coeff = []
             for (var j = 0; j < this.dimensions; j++) {
                 if (i === j) {
-                    coeff.push(1) // FIXME
+                    coeff.push(1)
                 } else {
-                    coeff.push(0.1) // FIXME
+                    coeff.push(0.2 + 0.2 * Math.random())
                 }
             }
             coefficients.push(coeff)
@@ -101,7 +88,16 @@ export default {
             for (var i = 0; i < this.dimensions; i++) {
                 delta.push(this.target[i] - this.current[i])
             }
-            return delta
+
+            const result = []
+            for (var i = 0; i < this.dimensions; i++) {
+                let d = 0
+                for (var j = 0; j < this.dimensions; j++) {
+                    d += delta[j] * this.coefficients[i][j]
+                }
+                result.push(d)
+            }
+            return result
         },
         graphs() {
             const graphs = []
