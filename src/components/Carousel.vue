d8bfd4c6b16c30010fed71d3ac481115ccb9fbef
Sampo Juustila <sampo.juustila@iki.fi>
2019-06-25 12:05:55 +0300

    feat: update carousel page

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -4,18 +4,19 @@
     <v-ons-carousel fullscreen swipeable auto-scroll overscrollable
       :index.sync="carouselIndex"
     >
-      <v-ons-carousel-item v-for="(value, key) in items" v-bind:key="key" :style="{backgroundColor: value.color}">
-        <div style="text-align: center; font-size: 30px; margin-top: 20px; color: #fff;">{{key}}</div>
+      <v-ons-carousel-item v-for="page in pages" v-bind:key="page.id" :style="{backgroundColor: page.color}">
+        <div style="text-align: center; font-size: 30px; margin-top: 20px; color: #fff;">{{page.title}}</div>
         <div style="text-align: center; margin-top: 50px;">
-          <span class="scan-item" v-bind:key="tool" v-for="tool in value.tools">
-            <v-ons-icon @click="scan(tool)" :icon="'fa-' + tool" size="3x"></v-ons-icon>
+          <span class="scan-item" v-for="tool in page.tools" v-bind:key="tool.title">
+            <v-ons-icon @click="scan(tool)" :icon="'fa-' + tool.icon" size="3x"></v-ons-icon><br>
+            <span class="scan-title">{{tool.title}}</span>
           </span>
         </div>
       </v-ons-carousel-item>
     </v-ons-carousel>
 
     <div :style="dots">
-      <span :index="dotIndex - 1" v-for="dotIndex in Object.keys(items).length" :key="dotIndex" style="cursor: pointer" @click="carouselIndex = dotIndex - 1">
+      <span :index="dotIndex - 1" v-for="dotIndex in Object.keys(pages).length" :key="dotIndex" style="cursor: pointer" @click="carouselIndex = dotIndex - 1">
         {{ carouselIndex === dotIndex - 1 ? '\u25CF' : '\u25CB' }}
       </span>
     </div>
@@ -38,26 +39,88 @@ import ScienceScanner from './ScienceScanner.vue'
 import FlappyDrone from './FlappyDrone.vue'
 import Locator from './Locator.vue'
 
+const PAGES = [
+  {
+    id: 'science',
+    title: 'Science',
+    role: 'role:science',
+    color: '#085078',
+    tools: [
+      {
+        icon: 'searchengin',
+        title: 'Scan artifact',
+        page: ScienceArtifactDetails,
+      },
+      {
+        icon: 'futbol',
+        title: 'Inspect object',
+        page: ScienceInspectObject,
+      },
+      {
+        icon: 'puzzle-piece',
+        title: 'Analyze',
+        page: ScienceAnalysis,
+      },
+      {
+        icon: 'cash-register',
+        title: 'Operate scanners',
+        page: ScienceScanner,
+      },
+    ],
+  },
+  {
+    id: 'engineering',
+    title: 'Engineering',
+    role: 'role:engineer',
+    color: '#373B44',
+    tools: [
+      {
+        icon: 'tools',
+        title: 'Repair system',
+        page: GameScanner,
+      },
+    ],
+  },
+  {
+    id: 'medic',
+    title: 'Medic',
+    role: 'role:medic',
+    color: '#D38312',
+    tools: [
+      {
+        icon: 'diagnoses',
+        title: 'Medical scan',
+        page: MedicalRecords,
+      },
+      {
+        icon: 'user-injured',
+        title: 'Diagnose injury',
+        page: MedicalDiagnosis,
+      },
+      {
+        icon: 'vial',
+        title: 'Take sample',
+        page: MedicalSample,
+      },
+      {
+        icon: 'cash-register',
+        title: 'Operate scanners',
+        page: MedicalScanner,
+      },
+    ],
+  }
+]
+
 export default {
   name: "Carousel",
   /* icons: 'magnet', 'id-card', 'car-battery', 'calculator', 'screwdriver', 'wrench'  */
   data() {
+    const groups = this.$store.state.user.user.groups;
+    const pages = PAGES.filter(page => groups.includes(page.role))
+
     return {
       carouselIndex: 0,
-      items: {
-        SCIENCE: {
-          color: '#085078',
-          tools: [ 'atom', 'dna', 'vial', 'flask', 'biohazard', 'chart-bar', 'eye-dropper', 'mortar-pestle', 'radiation', 'skull-crossbones', 'square-root-alt', 'temperature-high' ],
-        },
-        ENGINEERING: {
-          color: '#373B44',
-          tools: [ 'cogs', 'compass', 'microchip', 'globe', 'charging-station', 'hammer', 'project-diagram', 'rocket', 'ruler', 'satellite', 'sliders-h', 'tools' ],
-        },
-        MEDIC: {
-          color: '#D38312',
-          tools: [ 'medkit', 'microscope', 'syringe', 'diagnoses', 'x-ray', 'band-aid', 'brain', 'first-aid', 'heartbeat', 'notes-medical', 'pills', 'prescription-bottle' ]
-        },
-      },
+      pages,
       dots: {
         textAlign: 'center',
         fontSize: '30px',
@@ -69,39 +132,10 @@ export default {
       },
     }
   },
+
   methods: {
     scan(tool) {
-      if (tool === 'radiation') {
-        this.$store.commit('navigator/push', RadiationDetector)
-      } else if (tool === 'atom') {
-        this.$store.commit('navigator/push', GameScanner)
-      } else if (tool === 'dna') {
-        this.$store.commit('navigator/push', ScienceArtifactDetails)
-      } else if (tool === 'vial') {
-        this.$store.commit('navigator/push', ScienceInspectObject)
-      } else if (tool === 'flask') {
-        this.$store.commit('navigator/push', ScienceAnalysis)
-      } else if (tool === 'eye-dropper') {
-        this.$store.commit('navigator/push', ScienceScanner)
-      } else if (tool === 'project-diagram') {
-        this.$store.commit('navigator/push', LightsOut)
-      } else if (tool === 'chart-bar') {
-        this.$store.commit('navigator/push', GameTest)
-      } else if (tool === 'notes-medical') {
-        this.$store.commit('navigator/push', MedicalRecords)
-      } else if (tool === 'microscope') {
-        this.$store.commit('navigator/push', MedicalScanner)
-      } else if (tool === 'pills') {
-        this.$store.commit('navigator/push', MedicalDiagnosis);
-      } else if (tool === 'prescription-bottle' || tool === 'medkit') {
-        this.$store.commit('navigator/push', MedicalSample);
-      } else if (tool === 'satellite') {
-        this.$store.commit('navigator/push', FlappyDrone)
-      } else if (tool === 'compass') {
-        this.$store.commit('navigator/push', Locator)
-      }  else {
-        this.$store.commit('navigator/push', Scanner)
-      }
+      this.$store.commit('navigator/push', tool.page)
     }
   }
 }
@@ -111,7 +145,12 @@ export default {
   display: inline-block;
   margin: 20px;
   width: 80px;
-  height: 80px;
+  height: 100px;
   text-align: center;
+  overflow: hidden;
+}
+.scan-title {
+  display: inline-block;
+  margin-top: 0.8em;
 }
 </style>
