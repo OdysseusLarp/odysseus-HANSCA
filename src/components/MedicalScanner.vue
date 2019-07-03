52b7720de27b4adcfc155ecfe38050219cae4adc
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-07-03 16:41:12 +0300

    feat: require bio_id in medical scanner, add scan types

--- src/components/MedicalScanner.vue
+++ src/components/MedicalScanner.vue
@@ -2,19 +2,29 @@
 <template>
   <v-ons-page @show="show" @hide="hide">
     <toolbar-top />
-    <div class="container">
+    <div class="container no-top-margin">
         <h1>MEDICAL SCANNERS</h1>
-        <p class="has-id" v-if="bio_id">BIO ID OK</p>
-        <p class="no-bio-id" v-else-if="!standingInProgress && !tableInProgress">SCAN PATIENT BIO ID</p>
-        <button :disabled="standingInProgress || tableInProgress" type="button" class="button-standing-scanner" @click="startStandingScanner">
+        <p class="bio-id has-id" v-if="bio_id">BIO ID OK</p>
+        <p class="bio-id no-bio-id" v-else-if="!standingInProgress && !tableInProgress">SCAN PATIENT BIO ID<span class="required">*</span></p>
+        <label for="additional-type">SCAN TYPE<span class="required">*</span></label>
+        <v-ons-select name="additional-type" class="type-select" v-model="additional_type" @change="validateForm" :disabled="standingInProgress || tableInProgress">
+          <option v-for="option in typeOptions" :value="option.key" v-bind:key="option.key">
+            {{ option.text }}
+          </option>
+        </v-ons-select>
+        <label for="sample-description" class="label-description" v-if="additional_type === 'OTHER_SCAN'">DESCRIPTION<span class="required">*</span></label>
+        <span class="subtitle" v-if="additional_type === 'OTHER_SCAN'">(What area your scan is targeting)</span>
+        <textarea v-model="description" id="sample-description" v-if="additional_type === 'OTHER_SCAN'" @keyup="validateForm" :disabled="standingInProgress || tableInProgress" />
+        <button v-bind:class="{ 'in-progress': standingInProgress || tableInProgress }" :disabled="standingInProgress || tableInProgress || !isValid || !bio_id" type="button" class="button-standing-scanner" @click="startStandingScanner">
           {{ standingInProgress ? 'STANDING SCANNER IS SCANNING...' : 'START STANDING SCANNER' }}
         </button>
-        <button :disabled="tableInProgress || standingInProgress" type="button" class="button-table-scanner" @click="startTableScanner">
+        <button v-bind:class="{ 'in-progress': standingInProgress || tableInProgress }" :disabled="tableInProgress || standingInProgress || !isValid || !bio_id" type="button" class="button-table-scanner" @click="startTableScanner">
           {{ tableInProgress ? 'TABLE SCANNER IS SCANNING...' : 'START TABLE SCANNER' }}
         </button>
     </div>
   </v-ons-page>
 </template>
+
 <script>
 import { post } from 'axios';
 import { startWatch, cancelWatch, hasNfc } from '../nfc'
@@ -36,9 +46,26 @@ export default {
         standingInProgressTimeout: null,
         hasInput: !hasNfc(),
         bio_id: '', // Target person Bio ID
+        additional_type: 'XRAY_SCAN',
+        isValid: true,
+        description: '',
+        typeOptions: [
+            { key: 'XRAY_SCAN', text: 'X-Ray scan' },
+            { key: 'OTHER_SCAN', text: 'Other scan' },
+        ],
     }
   },
+  created() {
+      console.log('scanners in progress?', this.tableInProgress, this.standingInProgress);
+  },
   methods: {
+    validateForm(evt) {
+        if (evt && evt.key === 'Enter' && evt.target) {
+            evt.target.blur();
+        }
+        const description = this.description.trim();
+        this.isValid = this.bio_id && this.additional_type && (this.additional_type !== 'OTHER_SCAN' || description);
+    },
     startTableScanner() {
         if (this.tableInProgress) return;
         this.startScanner(TABLE_SCANNER_DMX_CHANNEL).then(success => {
@@ -61,6 +88,9 @@ export default {
         this.standingInProgress = false;
         this.tableInProgress = false;
         this.bio_id = '';
+        this.additional_type = 'XRAY_SCAN';
+        this.description = '';
+        this.validateForm();
     },
     startScanner(channel) {
         return post(`/dmx/event/${channel}`).then(() => {
@@ -85,9 +115,11 @@ export default {
             is_analysed: false,
             author_id: this.$store.state.user.user.id,
             type: 'MEDIC',
-            additional_type: scannerType,
+            additional_type: this.additional_type,
             bio_id: this.bio_id,
       };
+      const description = this.description.trim();
+      if (description) data.description = description;
       post('/operation', data).then(res => {
           console.log('created operation result for the scan', data);
       }).catch(err => {
@@ -101,6 +133,7 @@ export default {
             const id = message.split(':', 2)[1];
             this.bio_id = id;
             console.log('this bio id =>', this.bio_id);
+            this.validateForm();
         } else {
             this.$ons.notification.toast('Scanned tag is not a Bio ID', { timeout: 2500, animation: 'fall' });
         }
@@ -120,10 +153,25 @@ export default {
   },
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 $gray: #171717;
 $light-gray: #383838;
 $orange: #f4a140;
+
+.bio-id {
+  padding-top: 0;
+  margin-top: 0;
+  font-size: 1.2rem;
+  margin-bottom: 20px;
+}
+
+.label-description {
+    margin-top: 20px;
+}
+
+v-ons-select {
+    margin-bottom: 20px;
+}
 
 .has-id {
     color: rgb(88, 240, 88);
@@ -132,24 +180,35 @@ $orange: #f4a140;
     color: rgb(228, 78, 78);
 }
 
+.subtitle {
+    font-size: 0.75rem;
+}
+
+.container.no-top-margin {
+    margin-top: 0 !important;
+}
+
 button {
   background: darken($orange, 10);
   border: 0.1rem solid darken($orange, 25);
-  font-size: 1.6rem;
-  padding: 1.5rem 0.9rem;
+  font-size: 1.3rem;
   margin: 0.5rem;
   color: #fff;
-  margin-top: 2rem;
+  margin-top: 1rem;
   text-shadow: 0px 0px 3px rgba(0, 0, 0, .8);
 }
 button:disabled {
     background: darken($orange, 30);
-    animation: scanner-blinker 2s linear infinite;
 }
 
 @keyframes scanner-blinker {
     50% {
         background: darken($orange, 20);
     }
+}
+
+.in-progress {
+    background: darken($orange, 30);
+    animation: scanner-blinker 2s linear infinite;
 }
 </style>
