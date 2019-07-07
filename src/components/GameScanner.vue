f37dabba3484eded0faf1612fbc7a3abc318d7d1
Sampo Juustila <sampo.juustila@iki.fi>
2019-07-07 18:33:41 +0300

    feat: save game start time + duration in game data blob

--- src/components/GameScanner.vue
+++ src/components/GameScanner.vue
@@ -102,7 +102,8 @@ export default {
       defaultNotBroken: 'The system is operating nominally',
       debug: false,
       debugCount: 0,
-      gameLoader: () => undefined
+      gameLoader: () => undefined,
+      startTime: 0,
     }
   },
   methods: {
@@ -154,7 +155,11 @@ export default {
       console.log('getting game component');
       if (config.game in GAMES) {
         cancelWatch()
-        this.gameLoader = () => this.component = GAMES[config.game]
+        this.gameLoader = () => {
+          this.component = GAMES[config.game]
+          this.startTime = Date.now()
+          patchBlob('/data/' + this.game.type, this.game.id, { game_start: new Date().toISOString() })
+        }
         let condition = true
         if (this.config.preCondition) {
           condition = await this.checkPreCondition()
@@ -182,7 +187,8 @@ export default {
       this.state = 'game'
     },
     success() {
-      patchBlob('/data/' + this.game.type, this.game.id, { status: 'fixed' })
+      const duration = Date.now() - this.startTime
+      patchBlob('/data/' + this.game.type, this.game.id, { status: 'fixed', game_duration: Math.floor(duration/1000) })
       if (this.config.endDescription) {
         this.state = 'end'
       } else {
